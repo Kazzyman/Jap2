5d6abc94a29e5770319d85c8766608951aac3519
Richard Hart Woolley <rick.woolley@gmail.com>
2025-02-27 13:02:44 -0800

    Add files via upload

--- functionsFromMain.go
+++ functionsFromMain.go
@@ -72,3 +72,122 @@ func logRightZu2(userInput, promptField, actual_objective_type, objective string
 	log_right_andUpdateGame(promptField, userInput)
 
 }
+
+func beginGame() {
+	fmt.Println("What is your first name? (one word)")
+	_, _ = fmt.Scan(&nameOfPlayer)
+	List_of_game_types()
+	fmt.Println(colorRed + "Select a game" + colorReset)
+	_, _ = fmt.Scan(&type_of_game)
+	if type_of_game != "1" && type_of_game != "2" {
+		fmt.Printf("\n%s is not a valid entry\n", type_of_game)
+		fmt.Println(colorRed + "Select a game from the list (1-9)" + colorReset)
+		_, _ = fmt.Scan(&type_of_game)
+	}
+	fmt.Println(colorRed + "Enter proposed game duration, or 0 for a complete game & Points to console" + colorReset)
+	_, _ = fmt.Scan(&gameDuration)
+	if gameDuration > 0 {
+		// gameDuration will be set (below) to the combined length of the decks involved
+	} else {
+		if type_of_game == "1" {
+			gameDuration = 2*len(fileOfCardsHiraKata) + (2 * len(fileOfCardsEasyKanji)) + len(fileOfCardsKanjiHard)
+		}
+	}
+	switch type_of_game {
+	case "1": // hko Use Kata & Hira prompting (Roma objectives)
+		kata_hira = false
+		kata_roma = false
+		limitedToKataPrompts = true
+		limitedToHiraPrompts = true
+		limitedToRomaPrompts = false
+		limitedToDifficultDescriptions = false
+		limitedToSpelling = false
+	case "2": // konly Use only Kata prompting (mix Hira & Roma objectives)
+		gameDuration = 2 * len(fileOfCardsHiraKata)
+		kata_hira = false
+		kata_roma = false
+		limitedToKataPrompts = true
+		limitedToHiraPrompts = false
+		limitedToRomaPrompts = false
+		limitedToDifficultDescriptions = false
+		limitedToSpelling = false
+	case "3": // honly Use only Hira prompting (so only Roma responses)
+		gameDuration = len(fileOfCardsHiraKata)
+		kata_hira = false
+		kata_roma = false
+		limitedToHiraPrompts = true
+		limitedToKataPrompts = false
+		limitedToRomaPrompts = false
+		limitedToDifficultDescriptions = false
+		limitedToSpelling = false
+	case "4": // ronly Use only Romaji prompting (so only Hira responses)
+		gameDuration = len(fileOfCardsHiraKata)
+		kata_hira = false
+		kata_roma = false
+		limitedToRomaPrompts = true
+		limitedToKataPrompts = false
+		limitedToHiraPrompts = false
+		limitedToDifficultDescriptions = false
+		limitedToSpelling = false
+	case "5": // khSimplex : Use Kata prompting & Simplex Hira obj
+		gameDuration = len(fileOfCards_nonCompound)
+		kata_roma = false
+		kata_hira = false
+		limitedToKataPromptsAndSimplexHiraObj = true
+		limitedToKataPrompts = true
+		limitedToHiraPrompts = false
+		limitedToRomaPrompts = false
+		limitedToDifficultDescriptions = false
+		limitedToSpelling = false
+	case "6": // donly : Difficult descriptive prompting
+		gameDuration = len(dataMostDiff)
+		kata_hira = false
+		kata_roma = false
+		limitedToDifficultDescriptions = true
+		limitedToKataPrompts = false
+		limitedToHiraPrompts = false
+		limitedToRomaPrompts = true
+		limitedToSpelling = false
+	case "7": // kh : Use only kata_hira prompt_response
+		gameDuration = 2 * len(fileOfCardsHiraKata)
+		kata_roma = false
+		kata_hira = true
+		limitedToKataPrompts = false
+		limitedToHiraPrompts = false
+		limitedToRomaPrompts = false
+		limitedToDifficultDescriptions = false
+		limitedToSpelling = false
+	case "8": // rhSimplex : Use kata prompts w/Simplex Hira
+		gameDuration = len(fileOfCards_nonCompound)
+		kata_hira = false // ::: should be true ??
+		kata_roma = false
+		limitedToRomaPromptsAndSimplexHiraObj = true // ::: ?
+		limitedToRomaPrompts = true
+		limitedToKataPrompts = false
+		limitedToHiraPrompts = false
+		limitedToDifficultDescriptions = false
+		limitedToSpelling = false
+	case "9": // kr : Use only kata_roma prompt_response
+		gameDuration = len(fileOfCardsHiraKata)
+		kata_hira = false
+		kata_roma = true
+		limitedToKataPrompts = false
+		limitedToHiraPrompts = false
+		limitedToRomaPrompts = false
+		limitedToDifficultDescriptions = false
+		limitedToSpelling = false
+	default: // "0" // mix ::: NOT SURE ABOUT ALL THESE FALSE SETTINGS FOR THE MIX CASE ??
+		gameDuration = 3*len(fileOfCardsHiraKata) + (2 * len(fileOfCardsEasyKanji)) + len(fileOfCardsKanjiHard) // ::: 3 *
+		kata_hira = false
+		kata_roma = false
+		limitedToKataPrompts = false
+		limitedToHiraPrompts = false
+		limitedToRomaPrompts = false
+		limitedToDifficultDescriptions = false
+		limitedToSpelling = false
+		// ::: nothing limited means prompt with a mix of all three
+	}
+	display_limited_gaming_dir_list()
+	now_using_game_duration_set_by_game_type = true
+	the_game_begins()
+}
